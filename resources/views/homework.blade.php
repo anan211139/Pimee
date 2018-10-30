f5e70020b655004d4d812f53fff8d40f0f182ffc
anan <parichaya211139@gmail.com>
2018-10-31 03:27:01 +0700

    edit homework page

--- resources/views/homework.blade.php
+++ resources/views/homework.blade.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>พี่หมีติวเตอร์</title>
         <link rel="stylesheet" href="../css/studentinfo.css" />
-        <link rel="stylesheet" href="../css/leaderboard.css" />
+        <link rel="stylesheet" href="../css/homework.css" />
         <link href="https://fonts.googleapis.com/css?family=Athiti|Kanit|Mitr|Roboto" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
         <link rel="shortcut icon" href="picture/bear_N.png">
@@ -15,64 +15,47 @@
             <input type="text" name="line_code" id="useridprofilefield" style ="display:none;"></p>
             <div class="tabs">
                 <div class="tab-2 friend">
-                    <label for="tab2-1">เพื่อน</label>
+                    <label for="tab2-1">ยังไม่ได้ทำ</label>
                     <input id="tab2-1" name="tabs-two" type="radio" checked="checked">
                     <div>
-                        <p>กำลังพัฒนา</p> 
-                        <!-- <p id="useridfield"></p> -->
+                        <table>
+                            <tr>
+                                <th>วิชา</th>
+                                <th>เรื่อง</th> 
+                                <th>คุณครู</th>
+                                <th>กำหนดส่ง</th>
+                            </tr>
+                            <tr>
+                                <td>ffff</td>
+                                <td>ff</td> 
+                                <td>ff</td>
+                                <td>ff</td>
+                            </tr>
+                          
+                        </table>
                     </div>
                 </div>
                 <div class="tab-2 all">
-                    <label for="tab2-2">ทั้งระบบ</label>
+                    <label for="tab2-2">ทำเสร็จแล้ว</label>
                     <input id="tab2-2" name="tabs-two" type="radio">
                     <div>
                         <table>
-                            @foreach($top_students as $top_students)
-                                <?php
-                                    $class_rank = "o";
-                                    if($top_students->rank == 1){
-                                        $class_rank = "st_rank";
-                                    }else if ($top_students->rank == 2) {
-                                        $class_rank = "nd_rank";
-                                    }else if ($top_students->rank == 3) {
-                                        $class_rank = "th_rank";
-                                    }else{
-                                        $class_rank = "other_rank";
-                                    }
-                                ?>
-                                <tr class="<?php echo($class_rank) ?>">
-                                    <td><img class="img_leader" src="{{ $top_students->local_pic }}"/></td>
-                                    <td>{{ $top_students->name }}</td> 
-                                    <td>{{ $top_students->point }}</td>
-                                    <?php if($top_students->rank <= 3) {?>
-                                            <td class="trophy"><img src="https://pimee.softbot.ai/img/{{$top_students->rank}}.png"></td>
-                                    <?php }else{ ?>
-                                            <td>{{ $top_students->rank }}</td>
-                                    <?php } ?>
-                                    
-                                </tr>
-                            @endforeach
+                            <tr>
+                                <th>วิชา</th>
+                                <th>เรื่อง</th> 
+                                <th>คุณครู</th>
+                                <th>กำหนดส่ง</th>
+                            </tr>
+                            <tr>
+                                <td>ffff</td>
+                                <td>ff</td> 
+                                <td>ff</td>
+                                <td>ff</td>
+                            </tr>
                         </table>
                     </div>
                 </div>
             </div>
         </div> 
-        <div class="lb-myself flex">
-            @foreach($rank_ms as $rank_ms)
-                <table class="ms">
-                    <tr>
-                        <td><img class="img_leader" src="{{ $rank_ms->local_pic }}"/></td>
-                        <td>{{ $rank_ms->name }}</td> 
-                        <td>{{ $rank_ms->point }}</td>
-                        <?php if($rank_ms->rank <= 3) {?>
-                                <td class="trophy"><img src="https://pimee.softbot.ai/img/{{$rank_ms->rank}}.png"></td>
-                        <?php }else{ ?>
-                                <td>{{ $rank_ms->rank }}</td>
-                        <?php } ?>
-                        <p id="useridprofilefield"></p>
-                    </tr>
-                </table>
-            @endforeach
-        </div>
     </body>
 </html>
