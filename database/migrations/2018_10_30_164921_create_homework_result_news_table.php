043f5a4904a995def576ae1886107b023bac10bd
anan <parichaya211139@gmail.com>
2018-10-30 16:53:59 +0700

    rename file homework_result

--- database/migrations/2018_10_30_164921_create_homework_result_news_table.php
+++ database/migrations/2018_10_30_164921_create_homework_result_news_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class HomeworkResult extends Migration
+class CreateHomeworkResultNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class HomeworkResult extends Migration
      */
     public function up()
     {
-        Schema::create('homework_results', function (Blueprint $table) {
+        Schema::create('homework_result_news', function (Blueprint $table) {
             $table->increments('id');
             $table->string('line_code');
             $table->Integer('examgroup_id');
             $table->Integer('total');
             $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class HomeworkResult extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('homework_result_news');
     }
 }
