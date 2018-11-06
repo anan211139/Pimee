154e6b703d9ffcf868071ad00d22e6e1ab42778e
anan <parichaya211139@gmail.com>
2018-11-06 14:11:03 +0700

    edit column in table homework_logs

--- app/homework_log.php
+++ app/homework_log.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class homework_log extends Model
 {
-    protected $fillable = ['line_code', 'examgroup_id', 'exam_id','answer','is_correct'];
+    protected $fillable = ['line_code', 'group_hw_id', 'exam_id','answer','is_correct'];
 }
--- database/migrations/2018_10_30_151420_create_homework_logs_table.php
+++ database/migrations/2018_10_30_151420_create_homework_logs_table.php
@@ -16,7 +16,7 @@ class CreateHomeworkLogsTable extends Migration
         Schema::create('homework_logs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('line_code');
-            $table->Integer('examgroup_id');
+            $table->Integer('group_hw_id');
             $table->Integer('exam_id');
             $table->smallInteger('answer')->nullable();
             $table->boolean('is_correct')->nullable();
